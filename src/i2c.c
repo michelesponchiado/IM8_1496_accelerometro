4542074fa9cd51060f90fc49dace52eedfdd55c9
Michele Sponchiado <mikesponk@gmail.com>
2017-04-05 15:36:15 +0200

    added stats to check if i2c is in error

--- src/i2c.c
+++ src/i2c.c
@@ -474,14 +474,62 @@ int main(void)
 }
 #endif
 
+#define def_center_acc_value 2048
+typedef struct _type_acc_io_stats
+{
+	int32_t min, max, avg;
+	uint32_t idx;
+	int32_t accum;
+	int32_t values[32];
+	uint32_t nsamples;
+}type_acc_io_stats;
+
 typedef struct _type_acc_io
 {
 	uint16_t Xacc;
 	uint16_t Yacc;
+	int16_t Xacc_unbiased;
+	int16_t Yacc_unbiased;
 	int retcodeTransfer;
 	int retcodeSend;
 	int retcodeCmdRead;
+	I2C_XFER_T xfer;
+	uint8_t txBuff[16];
+	uint8_t rxBuff[16];
+
+	type_acc_io_stats statsX, statsY;
+	uint32_t numerr_send1, numerr_send2;
 }type_acc_io;
+
+static void init_stats(type_acc_io_stats * p)
+{
+	memset(p, 0, sizeof(*p));
+	p->min = INT32_MAX;
+	p->max = INT32_MIN;
+}
+
+static void update_stats(type_acc_io_stats * p, int16_t new_value)
+{
+#define def_max_num_elem_values (sizeof(p->values) / sizeof(p->values[0]))
+	uint32_t idx = p->idx;
+	if (idx >= def_max_num_elem_values)
+	{
+		idx = 0;
+	}
+	p->accum -= p->values[p->idx];
+	p->accum += new_value;
+	p->values[p->idx] = new_value;
+	p->avg = p->accum / def_max_num_elem_values;
+	if (new_value < p->min)
+	{
+		p->min = new_value;
+	}
+	if (new_value > p->max)
+	{
+		p->max = new_value;
+	}
+	p->nsamples++;
+}
 
 static type_acc_io acc_io;
 
@@ -489,53 +537,66 @@ void test_i2c(void)
 {
 	i2c_app_init(I2C0, SPEED_100KHZ);
 
-	static I2C_XFER_T xfer;
-	memset(&xfer, 0, sizeof(xfer));
-
-	uint8_t txBuff[16];
-	memset(txBuff, 0, sizeof(txBuff));
-
-	uint8_t rxBuff[16];
-	memset(rxBuff, 0, sizeof(rxBuff));
-
-	xfer.txBuff = &txBuff[0];
-	xfer.rxBuff = &rxBuff[0];
-	xfer.slaveAddr = 0x10;
-	xfer.rxSz = 0;
-	xfer.txSz = 0;
 	memset(&acc_io, 0, sizeof(acc_io));
+	init_stats(&acc_io.statsX);
+	init_stats(&acc_io.statsY);
+
+	acc_io.xfer.txBuff = &acc_io.txBuff[0];
+	acc_io.xfer.rxBuff = &acc_io.rxBuff[0];
+	acc_io.xfer.slaveAddr = 0x10;
+	acc_io.xfer.rxSz = 0;
+	acc_io.xfer.txSz = 0;
 
 
 	while(1)
 	{
 
-		xfer.txBuff = &txBuff[0];
-		xfer.txSz = 2;
-		xfer.rxSz = 0;
-		txBuff[0] = 0; // select register 0
-		txBuff[1] = 0; // select latch acceleration data
-		xfer.rxBuff = &rxBuff[0];
-		acc_io.retcodeTransfer = Chip_I2C_MasterTransfer(i2cDev, &xfer);
+		// latch the new value
+		acc_io.xfer.txBuff = &acc_io.txBuff[0];
+		acc_io.xfer.txSz = 2;
+		acc_io.xfer.rxSz = 0;
+		acc_io.txBuff[0] = 0; // select register 0
+		acc_io.txBuff[1] = 0; // select latch acceleration data
+		acc_io.xfer.rxBuff = &acc_io.rxBuff[0];
+		acc_io.retcodeTransfer = Chip_I2C_MasterTransfer(i2cDev, &acc_io.xfer);
+		if (acc_io.retcodeTransfer)
+		{
+			acc_io.numerr_send1++;
+		}
 
-		xfer.txSz = 1;
-		txBuff[0] = 1; // select register 1
-		xfer.rxSz = 4; // expect 4 data back: XH, XL, YH, YL
-		xfer.rxBuff = &rxBuff[0];
-		xfer.txBuff = &txBuff[0];
-		memset(rxBuff, 0, sizeof(rxBuff));
-		acc_io.retcodeSend =  Chip_I2C_MasterTransfer(i2cDev, &xfer);
+		// read the current accelerations values
+		acc_io.xfer.txSz = 1;
+		acc_io.txBuff[0] = 1; // select register 1
+		acc_io.xfer.rxSz = 4; // expect 4 data back: XH, XL, YH, YL
+		acc_io.xfer.rxBuff = &acc_io.rxBuff[0];
+		acc_io.xfer.txBuff = &acc_io.txBuff[0];
+		acc_io.retcodeTransfer =  Chip_I2C_MasterTransfer(i2cDev, &acc_io.xfer);
+		if (acc_io.retcodeTransfer)
+		{
+			acc_io.numerr_send2++;
+		}
 
 		{
-			uint16_t Xacc = rxBuff[0];
+			uint16_t Xacc = acc_io.rxBuff[0];
 			Xacc <<= 8;
-			Xacc |=  rxBuff[1];
+			Xacc |=  acc_io.rxBuff[1];
 			acc_io.Xacc = Xacc;
+			int16_t Xacc_unbiased;
+			Xacc_unbiased = Xacc;
+			Xacc_unbiased -= def_center_acc_value;
+			acc_io.Xacc_unbiased = Xacc_unbiased;
+			update_stats(&acc_io.statsX, Xacc_unbiased);
 		}
 		{
-			uint16_t Yacc = rxBuff[2];
+			uint16_t Yacc = acc_io.rxBuff[2];
 			Yacc <<= 8;
-			Yacc |=  rxBuff[3];
+			Yacc |=  acc_io.rxBuff[3];
 			acc_io.Yacc = Yacc;
+			int16_t Yacc_unbiased;
+			Yacc_unbiased = Yacc;
+			Yacc_unbiased -= def_center_acc_value;
+			acc_io.Yacc_unbiased = Yacc_unbiased;
+			update_stats(&acc_io.statsY, Yacc_unbiased);
 		}
 	}
 
